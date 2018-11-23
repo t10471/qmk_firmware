5efa449c39d8f26e7f56ec33e1cf5c28197b4e16
t10471 <t104711202@gmail.com>
2018-11-24 05:16:20 +0900

    fix helix pico pro micro left moge

--- keyboards/helix/pico/keymaps/t10471/bk_keymap.c
+++ keyboards/helix/pico/keymaps/t10471/bk_keymap.c
@@ -66,46 +66,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
 [_QWERTY] = LAYOUT( \
-      KC_BSPC, KC_P,    KC_O,    KC_I,    KC_U,    KC_Y,                      KC_T,   KC_R,   KC_E,    KC_W,    KC_Q,    KC_ESC,  \
-      KC_QUOT, KC_SCLN, KC_L,    KC_K,    KC_J,    KC_H,                      KC_G,   KC_F,   KC_D,    KC_S,    KC_A,    KC_TAB,  \
-      KC_ENT,  KC_SLSH, KC_DOT,  KC_COMM, KC_M,    KC_N,                      KC_B,   KC_V,   KC_C,    KC_X,    KC_Z,    KC_LSFT, \
-      KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT, KANAT,   RAISE, KC_SPC,   KC_SPC,   LOWER,  MOUSE,  _______, KC_LALT, KC_LGUI, KC_LCTL  \
+      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+      KC_LCTL, KC_LALT, KC_LGUI, _______, MOUSE,   LOWER,   KC_SPC,  KC_SPC,  RAISE,   KANAT,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   ~  |   !  |   @  |   #  |   $  |   %  |             |   ^  |   &  |   *  |   (  |   )  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             |      |   _  |   +  |   {  |   }  |  |   |
+   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |      |   _  |   +  |   {  |   }  |  |   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             |      |   -  |   =  |   [  |   ]  |  \   |
+   * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |             |      |      | PrSc | Home | End  |      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
+   * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT( \
-      KC_DEL,  KC_RPRN, KC_LPRN, KC_ASTR, KC_AMPR, KC_CIRC,                   KC_PERC, KC_DLR,  KC_HASH, KC_AT,   KC_EXLM, KC_TILD, \
-      KC_PIPE, KC_RCBR, KC_LCBR, KC_PLUS, KC_UNDS, _______,                   _______, _______, _______, _______, _______, _______, \
-      KC_BSLS, KC_RBRC, KC_LBRC, KC_EQL,  KC_MINS, _______,                   _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL, \
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
+      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    _______, _______, KC_PSCR, KC_HOME, KC_END,  _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   /* Raise
    * ,-----------------------------------------.             ,-----------------------------------------.
    * |   `  |   1  |   2  |   3  |   4  |   5  |             |   6  |   7  |   8  |   9  |   0  | Del  |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |
+   * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |             |      |   -  |   =  |   [  |   ]  |  \   |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * |      |      |      |      |      |      |             | PrSc |PageDn|PageUp| PrSc | Home | End  |
+   * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |             |      |      | PrSc |PageDn|PageUp|      |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT( \
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-      KC_F12,  KC_F11,  KC_F10,  KC_F9,   KC_F8,   KC_F7,                     KC_F6,   KC_F5,   KC_F4,   KC_F3,   KC_F2,   KC_F1,   \
-      KC_END,  KC_HOME, KC_PSCR, KC_PGDN, KC_PGUP, _______,                   _______, _______, _______, _______, _______, _______, \
-      KC_MPLY, KC_VOLU, KC_VOLD, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    _______, _______, KC_PSCR, KC_PGDN, KC_PGUP, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   /* Mouse
@@ -120,10 +120,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_MOUSE] =  LAYOUT( \
-      _______, _______, KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L,                   KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, KC_LGUI, \
-      _______, _______, KC_MS_R, KC_MS_U, KC_MS_D, KC_MS_L,                   KC_BTN5, KC_BTN4, KC_ACL2, KC_ACL1, KC_ACL0, KC_LSFT, \
-      _______, _______, KC_RGHT, KC_UP,   KC_DOWN, KC_LEFT,                   _______, _______, _______, _______, _______, _______, \
-      _______, _______, KC_BTN5, KC_BTN4, KC_BTN3, KC_BTN2, KC_BTN1, _______, _______, _______, _______, _______, _______, _______  \
+      KC_LGUI, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3,                   KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______, _______, \
+      KC_LSFT, KC_ACL0, KC_ACL1, KC_ACL2, KC_BTN4, KC_BTN5,                   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  _______, _______, \
+      _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
+      _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, KC_BTN5, _______, _______ \
       )
 };
 
@@ -218,7 +218,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case EISU:
       if (record->event.pressed) {
-        register_code(KC_LANG2);
+        if(keymap_config.swap_lalt_lgui==false){
+          register_code(KC_LANG2);
+        }else{
+          SEND_STRING(SS_LALT("`"));
+        }
       } else {
         unregister_code(KC_LANG2);
       }
@@ -226,7 +230,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case KANA:
       if (record->event.pressed) {
-        register_code(KC_LANG1);
+        if(keymap_config.swap_lalt_lgui==false){
+          register_code(KC_LANG1);
+        }else{
+          SEND_STRING(SS_LALT("`"));
+        }
       } else {
         unregister_code(KC_LANG1);
       }
@@ -251,10 +259,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case KANAT:
       if (record->event.pressed) {
-        if(KANA_STATUS == EISU) {
-          register_code(KC_LANG1);
-        } else {
-          register_code(KC_LANG2);
+        if(keymap_config.swap_lalt_lgui==false){
+          if(KANA_STATUS == EISU) {
+            register_code(KC_LANG1);
+          } else {
+            register_code(KC_LANG2);
+          }
+        }else{
+          SEND_STRING(SS_LALT("`"));
         }
       } else {
         if(KANA_STATUS == EISU) {
